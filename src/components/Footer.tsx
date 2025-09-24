8a3c7aae1176ce4c3cdeb291bac989b04fa1b5de
MARTIN BOSHKOSKI <55806675+martinboshkoski@users.noreply.github.com>
2025-09-24 21:30:13 +0200

    footer background

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import ldpLogo from '@/assets/ldp-logo-transparent.png';
 
 const Footer = () => {
   return (
-    <footer className="bg-foreground text-background py-12">
+    <footer className="bg-foreground text-background py-2">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center space-y-6">
           {/* Logo */}
